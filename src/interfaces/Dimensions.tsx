bfe277f0d903859bedf6ea139bd0c32a4ebf331d
Aldrin Mathew <admin@aldrinmathew.com>
2021-06-08 22:05:53 +0530

    Added Padding & corresponding Interfaces
    Added abstraction for Padding and also interfaces for dimensions.
    SideInterface defines Top, Bottom, Left and Right.
    DiagonalInterface defines TopLeft, TopRight, BottomLeft, BottomRight.
    SymmetryInterface defines horizontal and vertical.

---
+++ src/interfaces/Dimensions.tsx
@@ -0,0 +1,18 @@
+export interface SideInterface {
+    top?: number,
+    left?: number,
+    right?: number,
+    bottom?: number,
+}
+
+export interface DiagonalInterface {
+    topLeft?: number,
+    topRight?: number,
+    bottomLeft?: number,
+    bottomRight?: number,
+}
+
+export interface SymmetryInterface {
+    horizontal?: number,
+    vertical?: number,
+}
